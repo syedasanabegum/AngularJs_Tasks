254e69cff0dd9cc5a9a6babef0827e5841e76def
syedasanabegum <syedasana1009@gmail.com>
2023-06-15 23:25:06 +0530

    MyAppDeleted

--- MyApp/app.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module('myApp', ['ngRoute','ui.grid','ui.grid.pagination']);
-
-
-
-app.controller('MainController', function($scope, $location,) {
-  $scope.users = {}; // To store user data
-  console.log('app');
-});
-
